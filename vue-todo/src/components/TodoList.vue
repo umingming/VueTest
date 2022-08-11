cbf9dcc7aca5fd2bd42dd285d2354792618e58b0
umingming <u_0112@naver.com>
2022-08-11 10:43:56 +0900

    Update list style

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,15 +1,83 @@
 <template>
     <div>
-        list
+        <ul>
+            <!-- vs코드라서 바인딩해야 되는 것, 자체에서 오류내는 거임. -->
+            <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
+                <i class="checkBtn fas fa-check" v-on:click="toggleComplete"></i>
+                {{ todoItem }}
+                <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+                    <i class="fas fa-trash-alt"></i>
+                </span>
+                <!-- <button v-on:click>delete</button> -->
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
 export default {
+    data: function() {
+        return {
+            todoItems: []
+        }
+    },
+    methods: {
+        removeTodo: function(todoItem, index) {
+            console.log(todoItem, index);
+            localStorage.removeItem(todoItem); //브라우저 저장소 영역이라 화면에선 안 지워짐.
+            this.todoItems.splice(index, 1) //js 배열 이벤트로 특정 인덱스에서 하나 지울 수 있음.
+        },
+        toggleComplete: function() {
+            
+        }
+    },
+    created: function() {
+        if (localStorage.length > 0) {
+            for (var i = 0; i < localStorage.length; i++) {
+//                this.todoItems.push(localStorage.key(i));
+//                console.log(typeof localStorage.getItem(localStorage.key(i))); //문자열
+                //console.log(JSON.parse(localStorage.getItem(localStorage.key(i)))); //String 값을 json으로 변환
+                this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))))
+            }
+        }
+    }
 
 }
 </script>
 
-<style>
+<style scoped>
+ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+}
+li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem, 0;
+    margin-top: 0.2rem;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+}
+.checkBtn {
+    line-height: 45px;
+    color: #62ACDE;
+    margin-right: 5px;
+}
+.checkBtnCompleted {
+    color: #B3ADAD;
+}
+.textCompleted {
+    text-decoration: line-through;
+    color: #B3ADAD
+}
+.removeBtn {
+    margin-left: auto;
+    color: #DE4343;
+}
 
 </style>
