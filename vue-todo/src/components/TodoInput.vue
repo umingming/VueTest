25f33ca2e5d6a59179a373d4f9f60e263154788b
umingming <u_0112@naver.com>
2022-08-11 10:43:48 +0900

    Update list style

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -16,8 +16,13 @@ export default {
   },
   methods: {
     addTodo: function() {
-      localStorage.setItem(this.newTodoItem, this.newTodoItem);
-      this.clearInput();
+      if (this.newTodoItem !== '') {
+        var obj = {completed: false, item: this.newTodoItem};
+        localStorage.setItem(this.newTodoItem, JSON.stringify(obj)); //자바스크립트 객체를 스트링 값으로 변환.
+        //이렇게만 하면 새로고침해야 화면에 반영됨.
+        this.clearInput();
+      }
+//      localStorage.setItem(this.newTodoItem, obj);  //값을 로컬스트리지에서 확인할 수 없음. [object Object]로 감.
     },
     clearInput: function() {
       this.newTodoItem = '';
