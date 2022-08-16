f785d923a9f20fa416905e4881cb4a39b1062935
umingming <u_0112@naver.com>
2022-08-16 17:54:54 +0900

    Update remove method to vuex

--- to-do/src/components/TodoList.vue
+++ to-do/src/components/TodoList.vue
@@ -28,13 +28,15 @@ export default {
     props: ['propsdata'],
     methods: {
         removeTodo: function(todoItem, index) {
-            this.$emit('removeItem', todoItem, index);
+            // this.$emit('removeItem', todoItem, index);
+            this.$store.commit('removeOneItem', todoItem, index);
             console.log(todoItem, index);
             //localStorage.removeItem(todoItem.item); //브라우저 저장소 영역이라 화면에선 안 지워짐.
             //this.todoItems.splice(index, 1) //js 배열 이벤트로 특정 인덱스에서 하나 지울 수 있음.
         },
         toggleComplete: function(todoItem, index) {
-            this.$emit('toggleItem', todoItem, index);
+            //this.$emit('toggleItem', todoItem, index);
+            this.$store.commit('toggleOneItem', todoItem, index);
         }
     },
     // created: function() {
--- to-do/src/store/store.js
+++ to-do/src/store/store.js
@@ -27,6 +27,19 @@ export const store = new Vuex.Store({
             const obj = { completed: false, item: todoItem };
             localStorage.setItem(todoItem, JSON.stringify(obj));
             state.todoItems.push(obj);
+        },
+        removeOneItem(state, todoItem, index) {
+            localStorage.removeItem(todoItem.item); //브라우저 저장소 영역이라 화면에선 안 지워짐.
+            state.todoItems.splice(index, 1) //js 배열 이벤트로 특정 인덱스에서 하나 지울 수 있음.
+        },
+        toggleOneItem(state, todoItem, index) {
+            //      todoItem.completed = !todoItem.completed;
+//            state.todoItems[index].completed = !state.todoItems[index].completed;
+            console.log(state.todoItems[index]);
+            localStorage.removeItem(todoItem.item);
+            localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+            console.log(index);
         }
     }    
+
 });
