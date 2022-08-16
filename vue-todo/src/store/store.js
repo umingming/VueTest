ab58dedbd5029fb1fdf5052f2fdcbbb56319da37
umingming <u_0112@naver.com>
2022-08-16 15:56:27 +0900

    Register Vuex in main.js

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -1,3 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+Vue.use(Vuex);
+
+// 밖에서 store 변수를 사용할 수 있음.
+export const store = new Vuex.Store({
+    state: {
+        headerText: 'TODO it!'
+    }
+});
