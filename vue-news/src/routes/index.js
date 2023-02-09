bd3498fe3cdd3fdee4366ecc17feda99caaa123b
umingming <u_0112@naver.com>
2023-02-09 17:15:15 +0900

    feat: Update router

--- vue-news/src/routes/index.js
+++ vue-news/src/routes/index.js
@@ -4,6 +4,8 @@ import ItemView from '../views/ItemView.vue';
 import UserView from '../views/UserView.vue';
 import NewsView from '../views/NewsView.vue';
 import createListView from '../views/CreateListView.js';
+import bus from '@/utils/bus';
+import { store } from '../store/index.js';
 
 Vue.use(VueRouter);
 
@@ -18,16 +20,19 @@ export const router = new VueRouter({  //export해줘야 다른 파일에서 참
             path: '/news', //url 주소
             name: "news",
             component: NewsView, //url 주소로 갔을 때 표시될 컴포넌트, 페이지 
+            beforeEnter,
         },
         {
             path: '/ask',
             name: "ask",
             component: createListView('AskView'),
+            beforeEnter,
         },
         {
             path: '/jobs',
             name: "jobs",
             component: createListView('JobsView'),
+            beforeEnter,
         },
         {
             path: '/item/:id',
@@ -39,3 +44,14 @@ export const router = new VueRouter({  //export해줘야 다른 파일에서 참
         },
     ]
 });
+
+function beforeEnter(to, from, next) {
+    bus.$emit('start:spinner');
+    setTimeout(() => {
+        store
+            .dispatch("FETCH_LIST", to.name)
+            .then(() => next())
+            .catch((error) => console.log(error));
+    }, 3000);
+}
+// 라우터 네비게이션을 쓰면 사용자를 위한 ui 도구가 필요함.
