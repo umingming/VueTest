beaf67152f7b0cbab377005eebbaa0384c04949a
umingming <u_0112@naver.com>
2023-02-14 08:30:06 +0900

    test: Apply common component

---
+++ vue-component/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+        <ul>
+            <item-component 
+                v-for="item in items" 
+                :key="item" 
+                :item="item" 
+            >
+            </item-component>
+        </ul>
+    </div>
+</template>
+
+<script>
+import ItemComponent from './components/ItemComponent';
+
+export default {
+    name: "App",
+    components: { ItemComponent },
+    data() {
+        return {
+            items: ['아이템1','아이템2','아이템3','아이템4','아이템5']
+        }
+    }
+};
+</script>
+
+<style>
+</style>
