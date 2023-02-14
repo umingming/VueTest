c35b3e0c35db303785dc0e0fc9caaf9740bd3511
umingming <u_0112@naver.com>
2023-02-14 09:17:38 +0900

    test

--- vue-component/src/App.vue
+++ vue-component/src/App.vue
@@ -13,16 +13,31 @@
         >
         <!-- v-model은 input 이벤트와 value값으로 이뤄짐. -->
         </check-box>
+        <fetch-data
+            url="https://jsonplaceholder.typicode.com/users/1"
+        >
+            <div
+                slot-scope="{ response, loading }"
+            >
+                <div v-if="!loading">
+                    {{ response }}
+                </div>
+                <div v-if="loading">
+                    Loading...
+                </div>
+            </div>
+        </fetch-data>
     </div>
 </template>
 
 <script>
 import ItemComponent from './components/ItemComponent';
 import CheckBox from './components/CheckBox';
+import FetchData from './components/FetchData';
 
 export default {
     name: "App",
-    components: { ItemComponent, CheckBox },
+    components: { ItemComponent, CheckBox, FetchData },
     data() {
         return {
             items: ['아이템1','아이템2','아이템3','아이템4','아이템5'],
