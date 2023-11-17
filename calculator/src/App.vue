9182f8f2c69f64c7437802e9ee47cb6183629c0c
umingming <u_0112@naver.com>
2023-11-17 18:06:15 +0900

    setup 연습

---
+++ calculator/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  {{ test }}
+  <div>
+    <div>
+      <button>%</button>
+      <button>CE</button>
+      <button>C</button>
+      <button>X</button>
+    </div>
+    <div>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>*</button>
+    </div>
+    <div>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>-</button>
+    </div>
+    <div>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>+</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const test = ref("test");
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
