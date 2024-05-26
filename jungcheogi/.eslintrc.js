a7da003f4d3aa57f40b9193b0fa7369e6c138471
umingming <u_0112@naver.com>
2024-05-26 15:31:07 +0900

    feat: Add algorithm

--- jungcheogi/.eslintrc.js
+++ jungcheogi/.eslintrc.js
@@ -1,46 +1,34 @@
 // .eslintrc.js
 module.exports = {
-	root: true,
-	parserOptions: {
-		parser: "@babel/eslint-parser",
-		requireConfigFile: false, // <== ADD THIS
-	},
-	env: {
-		node: true,
-		browser: true,
-	},
-	extends: [
-		"plugin:vue/vue3-essential",
-		"eslint:recommended",
-		"@vue/eslint-config-prettier",
-	],
-	rules: {
-		"prettier/prettier": [
-			"error",
-			// 아래 규칙들은 개인 선호에 따라 prettier 문법 적용
-			// https://prettier.io/docs/en/options.html
-			{
-				singleQuote: false,
-				semi: true,
-				useTabs: true,
-				tabWidth: 4,
-				trailingComma: "all",
-				printWidth: 80,
-				bracketSpacing: true,
-				arrowParens: "avoid",
-				endOfLine: "auto",
-			},
-		],
-	},
-	overrides: [
-		{
-			files: [
-				"**/__tests__/*.{j,t}s?(x)",
-				"**/tests/unit/**/*.spec.{j,t}s?(x)",
-			],
-			env: {
-				jest: true,
-			},
-		},
-	],
+    root: true,
+    parserOptions: {
+        parser: "@babel/eslint-parser",
+        requireConfigFile: false, // <== ADD THIS
+    },
+    env: {
+        es6: true,
+        node: true,
+        browser: true,
+    },
+    extends: [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "@vue/eslint-config-prettier",
+        "prettier",
+    ],
+    plugins: ["prettier"],
+    rules: {
+        "prettier/prettier": "error",
+    },
+    overrides: [
+        {
+            files: [
+                "**/__tests__/*.{j,t}s?(x)",
+                "**/tests/unit/**/*.spec.{j,t}s?(x)",
+            ],
+            env: {
+                jest: true,
+            },
+        },
+    ],
 };
--- jungcheogi/src/App.vue
+++ jungcheogi/src/App.vue
@@ -1,201 +1,211 @@
 <template>
-	<div class="subject-group">
-		<button
-			v-for="key in 5"
-			:key="key"
-			:class="{ on: selectedSubject === key }"
-			@click="selectSubject(key)"
-		>
-			{{ key }}
-		</button>
-	</div>
-	<div
-		v-if="currentQuestion"
-		:class="{ correct: currentQuestion.IS_CORRECT }"
-	>
-		<span id="question-mark">
-			{{ questionMark }}
-			<button @click="shuffleQuestionList">Shuffle</button>
-		</span>
-		<div class="question-box">
-			{{ currentQuestion.QUESTION }}
-		</div>
-		<span id="serial">
-			{{ currentQuestion.ITERATION }},
-			{{ currentQuestion.NO }}
-		</span>
-		<div class="answer-box">
-			<input ref="answerRef" @keyup.enter="checkAnswer" />
-			<button v-if="!isAnswerEntered" @click="checkAnswer">Enter</button>
-			<button v-else @click="passAnswer">Next</button>
-		</div>
-		<div class="info-box" v-if="isAnswerEntered">
-			<div v-if="currentQuestion.IS_CORRECT">맞았다! 잘했다!</div>
-			<div v-else>틀렸다.</div>
-			<div>
-				정답: <span>{{ currentQuestion.ANSWER }}</span>
-			</div>
-		</div>
-	</div>
-	<div v-else class="success">{{ selectedSubject }} 과목 완료!🎉</div>
+    <div class="subject-group">
+        <button
+            v-for="key in 5"
+            :key="key"
+            :class="{ on: selectedSubject === key }"
+            @click="selectSubject(key)"
+        >
+            {{ key }}
+        </button>
+    </div>
+    <div
+        v-if="currentQuestion"
+        :class="{ correct: currentQuestion.IS_CORRECT }"
+    >
+        <span id="question-mark">
+            {{ questionMark }}
+            <button @click="shuffleQuestionList">Shuffle</button>
+        </span>
+        <div class="question-box">
+            {{ currentQuestion.QUESTION }}
+        </div>
+        <span id="serial">
+            {{ currentQuestion.ITERATION }},
+            {{ currentQuestion.NO }}
+        </span>
+        <div class="answer-box">
+            <input ref="answerRef" @keyup.enter="checkAnswer" />
+            <button v-if="!isAnswerEntered" @click="checkAnswer">Enter</button>
+            <button v-else @click="passAnswer">Next</button>
+        </div>
+        <div class="info-box" v-if="isAnswerEntered">
+            <div v-if="currentQuestion.IS_CORRECT">맞았다! 잘했다!</div>
+            <div v-else>틀렸다.</div>
+            <div>
+                정답:
+                <span>{{ currentQuestion.ANSWER }}</span>
+            </div>
+        </div>
+    </div>
+    <div v-else class="success">{{ selectedSubject }} 과목 완료!🎉</div>
 </template>
 
 <script>
 import { computed, nextTick, onMounted, ref } from "vue";
 
-import { QUESTIONS } from "./plugin/constants";
+import { QUESTIONS } from "./plugin/algorithm";
 
 export default {
-	name: "App",
-	components: {},
-	setup() {
-		//============================== Subject
-		const selectedSubject = ref(0);
+    name: "App",
+    components: {},
+    setup() {
+        //============================== Subject
+        const selectedSubject = ref(0);
 
-		function selectSubject(key) {
-			selectedSubject.value = key;
-			changeQuestionList();
-		}
+        function selectSubject(key) {
+            selectedSubject.value = key;
+            changeQuestionList();
+        }
 
-		onMounted(() => selectSubject(1));
+        onMounted(() => selectSubject(1));
 
-		//============================== Question
-		const questionList = ref([]);
-		const currentQuestion = computed(() =>
-			questionList.value.find(question => !question.IS_ENTERED),
-		);
-		const questionMark = computed(() => {
-			const { length: total } = questionList.value;
-			const { length: index } = questionList.value.filter(
-				question => question.IS_CORRECT,
-			);
-			return `${index}/${total}`;
-		});
+        //============================== Question
+        const questionList = ref([]);
+        const currentQuestion = computed(() =>
+            questionList.value.find(question => !question.IS_ENTERED)
+        );
+        const questionMark = computed(() => {
+            const { length: total } = questionList.value;
+            const { length: index } = questionList.value.filter(
+                question => question.IS_CORRECT
+            );
+            return `${index}/${total}`;
+        });
 
-		function changeQuestionList() {
-			questionList.value = QUESTIONS.filter(
-				({ SUBJECT }) => SUBJECT === selectedSubject.value,
-			);
+        function changeQuestionList() {
+            questionList.value = QUESTIONS.filter(
+                ({ SUBJECT }) => SUBJECT === selectedSubject.value
+            );
 
-			if (currentQuestion.value) {
-				nextTick(() => initAnswer());
-			}
-		}
+            if (currentQuestion.value) {
+                nextTick(() => initAnswer());
+            }
+        }
 
-		function shuffleQuestionList() {
-			questionList.value.sort(() => Math.random() - Math.random());
-			initAnswer();
-		}
+        function shuffleQuestionList() {
+            questionList.value.sort(() => Math.random() - Math.random());
+            initAnswer();
+        }
 
-		//============================== Answer
-		const answerRef = ref(null);
-		const currentAnswer = computed(() => currentQuestion.value.ANSWER);
-		const isAnswerEntered = ref(false);
+        //============================== Answer
+        const answerRef = ref(null);
+        const currentAnswer = computed(() => currentQuestion.value.ANSWER);
+        const isAnswerEntered = ref(false);
 
-		function initAnswer() {
-			answerRef.value.value = "";
-			answerRef.value.focus();
-			isAnswerEntered.value = false;
-		}
+        function initAnswer() {
+            answerRef.value.value = "";
+            answerRef.value.focus();
+            isAnswerEntered.value = false;
+        }
 
-		function checkAnswer() {
-			if (isAnswerEntered.value) {
-				passAnswer();
-				return;
-			}
-			const { value } = answerRef.value;
-			currentQuestion.value.IS_CORRECT =
-				validateAnswer(currentAnswer.value) === validateAnswer(value);
+        function checkAnswer() {
+            if (isAnswerEntered.value) {
+                passAnswer();
+                return;
+            }
+            const { value } = answerRef.value;
+            currentQuestion.value.IS_CORRECT =
+                validateAnswer(currentAnswer.value) === validateAnswer(value);
 
-			isAnswerEntered.value = true;
-		}
+            isAnswerEntered.value = true;
+        }
 
-		function passAnswer() {
-			if (currentQuestion.value.IS_CORRECT) {
-				currentQuestion.value.IS_ENTERED = true;
-			} else {
-				const index = questionList.value.findIndex(
-					question => question === currentQuestion.value,
-				);
-				const [question] = questionList.value.splice(index, 1);
-				questionList.value.push(question);
-			}
-			initAnswer();
-		}
+        function passAnswer() {
+            if (currentQuestion.value.IS_CORRECT) {
+                currentQuestion.value.IS_ENTERED = true;
+            } else {
+                const index = questionList.value.findIndex(
+                    question => question === currentQuestion.value
+                );
+                const [question] = questionList.value.splice(index, 1);
+                questionList.value.push(question);
+            }
+            initAnswer();
+        }
 
-		function validateAnswer(value) {
-			return String(value).toUpperCase().replace(/ |,/g, "");
-		}
+        function validateAnswer(value) {
+            return String(value).toUpperCase().replace(/ |,/g, "");
+        }
 
-		return {
-			// Subject
-			selectedSubject,
-			selectSubject,
+        return {
+            // Subject
+            selectedSubject,
+            selectSubject,
 
-			// Question
-			currentQuestion,
-			questionMark,
-			changeQuestionList,
-			shuffleQuestionList,
+            // Question
+            currentQuestion,
+            questionMark,
+            changeQuestionList,
+            shuffleQuestionList,
 
-			// Answer
-			answerRef,
-			isAnswerEntered,
-			checkAnswer,
-			validateAnswer,
-			passAnswer,
-		};
-	},
+            // Answer
+            answerRef,
+            isAnswerEntered,
+            checkAnswer,
+            validateAnswer,
+            passAnswer,
+        };
+    },
 };
 </script>
 
 <style>
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-	white-space: break-spaces;
-	line-height: 25px;
-	max-width: 800px;
-	margin: auto;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    white-space: break-spaces;
+    line-height: 25px;
+    max-width: 800px;
+    margin: auto;
 }
+
 .subject-group button {
-	width: 50px;
-	margin: 10px;
+    width: 50px;
+    margin: 10px;
 }
+
 .subject-group button.on {
-	font-weight: bold;
+    font-weight: bold;
 }
+
 #question-mark {
-	display: block;
-	text-align: right;
-	margin: 0px 30px 10px;
+    display: block;
+    text-align: right;
+    margin: 0px 30px 10px;
 }
+
 #serial {
-	display: block;
-	text-align: right;
-	margin: 0px 30px;
+    display: block;
+    text-align: right;
+    margin: 0px 30px;
 }
+
 .answer-box input {
-	margin-right: 10px;
-	width: calc(80% - 100px);
-	height: 20px;
+    margin-right: 10px;
+    width: calc(80% - 100px);
+    height: 20px;
 }
+
 .success {
-	margin-top: 10px;
-	font-weight: bold;
+    margin-top: 10px;
+    font-weight: bold;
 }
+
 .info-box {
-	margin-top: 10px;
-	color: red;
+    margin-top: 10px;
+    color: red;
 }
+
 .info-box span {
-	font-weight: 600;
+    font-weight: 600;
 }
+
 .correct .info-box {
-	color: blue;
+    color: blue;
 }
 </style>
