369d65b27e40cf3ca8e28dd2e2cc27e2961cb48d
umingming <u_0112@naver.com>
2025-03-16 22:35:51 +0900

    feat: Expand/Collapse 기능 구현

--- tailwind-test/src/App.vue
+++ tailwind-test/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="flex flex-col items-center justify-center pt-10">
     <TextBox />
+    <ExpandBox />
   </div>
 </template>
 
 <script lang="ts">
-import TextBox from './components/common/Textbox.vue';
+import TextBox from './components/common/TextBox.vue';
+import ExpandBox from './components/common/ExpandBox.vue';
 
 export default {
   name: 'App',
   components: {
     TextBox,
+    ExpandBox,
   },
 };
 </script>
---
+++ tailwind-test/src/components/common/ExpandBox.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="w-96 border">
+    <div class="h-10 bg-red-500" @click="toggleExpanded"></div>
+    <div
+      class="h-0 overflow-auto bg-blue-500 transition-all"
+      :class="{ '!h-40': isExpanded }"
+    >
+      글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이
+      글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이
+      글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이
+      글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이
+      글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이
+      글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이
+      글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이
+      글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이
+      글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이
+      글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이
+      글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이 써볼게용 글자를 많이
+      써볼게용 글자를 많이 써볼게용 글자를 많이 써볼게용
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue';
+
+export default {
+  name: 'ExpandBox',
+  setup() {
+    const isExpanded = ref(false);
+
+    const toggleExpanded = () => {
+      isExpanded.value = !isExpanded.value;
+    };
+
+    return {
+      isExpanded,
+      toggleExpanded,
+    };
+  },
+};
+</script>
